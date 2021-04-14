9229bae8fdf24f585ab72812f6761b6047718dae
Omkar Jadhav <omkarj@wemotiveforge.com>
2021-04-14 16:44:46 +0530

    new- added cursor pointer to pagination span

--- src/components/ui/sidedrawer/sidedrawer.js
+++ src/components/ui/sidedrawer/sidedrawer.js
@@ -695,7 +695,7 @@ const SideiDrawer=()=> {
                 <MoreVertIcon  fontSize="small"/>
                 </IconButton >
                 </Tooltip>
-                <span style={{position:'relative',left:open ? '810px':ropen ? '940px':'980px'}} className={classes.paginationSpan} >1 of 1 </span>
+                <span style={{cursor:'pointer',position:'relative',left:open ? '810px':ropen ? '940px':'980px'}} className={classes.paginationSpan} >1 of 1 </span>
                 <Tooltip dense  title="Older" placement="bottom">
                 <ChevronRightIcon style={{position:'relative',left:open ? '891px' :ropen ? '1003px':'1050px'}} fontSize="small"/>
                 </Tooltip>
